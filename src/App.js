dffa0cb00ad701bbdcedf0e6169986bc6364c4ce
Stenio Sousa <steniosousaf@gmail.com>
2023-03-31 13:25:02 -0300

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {  useState } from 'react';
 function App() {
+  const [query, setQuery] =useState('')
+  const [ response, setResponse] = useState('')
+  const { Configuration, OpenAIApi } = require("openai");
+  const configuration = new Configuration({
+    apiKey: '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',
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  async function get(){
+    const {data} = await openai.createCompletion({
+      model: "text-babbage-001",
+      prompt: query,
+      temperature: 0,
+      max_tokens: 500,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    });
+    setResponse(data.choices[0].text)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://sistema.selletiva.com.br/images/logo.svg" className="App-logo" alt="logo" />
+        <input onChange={e =>setQuery(e.target.value)}></input>
+        <button onClick={get}>Send</button>
+        <br></br>
+        <h1>Resposta:</h1>
+        <p>{response}</p>
       </header>
     </div>
   );
